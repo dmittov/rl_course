6a7589c16d23283b221acea7be82ec4fd45c0289
Dmitry Mittov <mittov@gmail.com>
2022-05-16 08:15:12 +0200

    [week4] gamblers problem

--- week4/task_4_9.py
+++ week4/task_4_9.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from random import random
 from numpy.random import uniform
 import numpy as np
+import itertools
 
 
 @dataclass
@@ -16,7 +17,7 @@ class Environment:
         # players bank
         self.__init_bank = bank
         self.__target = target
-        self.__p = p
+        self.p = p
         self.reset()
 
     @property
@@ -30,7 +31,7 @@ class Environment:
     def step(self, stake: int) -> State:
         if (stake > self.bank) or (stake < 1):
             raise ValueError("Invalid stake")
-        diff = stake if uniform(0, 1) < self.__p else -stake
+        diff = stake if uniform(0, 1) < self.p else -stake
         self.bank += diff
         state = State(reward=0, is_done=False, bank=self.bank)
         if self.bank >= self.__target:
@@ -48,30 +49,76 @@ class Environment:
 
 
 class Agent:
-    def __init__(self, env):
+
+    def __init__(self, env, **kwargs):
         self.env = env
         self.states = np.arange(self.env.target + 1)
-        self.state_values = np.ones(self.env.target + 1) * 0.5
-        # rewards in states
-        self.rewards = np.zeros(self.env.target + 1)
-        self.rewards[self.env.target] = 1
+        self.state_values = uniform(0, 1, self.env.target + 1)
+        self.greedy_actions = kwargs.get("greedy_actions", np.ones(self.env.target + 1).astype(int))
+        self.eps = kwargs.get("eps", 0.1)
+        self.gamma = kwargs.get("gamma", 1.)
 
-        # self.greedy_actions = np.ones(self.env.target + 1)
+    def estimate_action(self, bank: int, stake: int) -> float:
+        v_win = 0.
+        v_lose = 0.
+        if (bank + stake) >= self.env.target:
+            v_win = 1.
+        else:
+            v_win = self.gamma * self.state_values[bank + stake]
+        if (bank - stake) <= 0:
+            v_lose = 0.
+        else:
+            v_lose = self.gamma * self.state_values[bank - stake]
+        action_value = (self.env.p * v_win) + ((1. - self.env.p) * v_lose)
+        return action_value
 
-    def value_estimation(self):
-        # get all possible actions
-        # get all states that can be caused by said actions (2 times as much)
-        # weight the value of those states by their probability + reward of state directly
-        # use thosew to estimate value of action
-        # decide on greedy action and update current value of state
-        actions = self.env.get_actions()
-        new_states = self.env.bank
+    def estimate_states(self, eps: float) -> float:
+        # non-terminal states
+        diff = 0.
+        for state in self.states[1:-1]:
+            # bet [1 .. state]
+            G = 0.
+            possible_actions = list(range(1, state + 1))
+            for stake in possible_actions:
+                # value of action
+                action_value = self.estimate_action(state, stake)
+                action_prob = (1. - eps) if stake == self.greedy_actions[state] else (eps / (len(possible_actions) - 1))
+                G += action_value * action_prob
+            diff = max(abs(self.state_values[state] - G), diff)
+            self.state_values[state] = G
+        return diff
 
-        #
+    def improve_policy(self) -> bool:
+        has_improvement = False
+        for state in self.states[1:-1]:
+            best_action = None
+            current_estimation = -1  # any reasonable estimation is >= 0 
+            possible_actions = list(range(1, state + 1))
+            for stake in possible_actions:
+                action_value = self.estimate_action(state, stake)
+                if action_value > current_estimation:
+                    current_estimation = action_value
+                    best_action = stake
+            if best_action != self.greedy_actions[state]:
+                has_improvement = True
+                self.greedy_actions[state] = best_action
+        return has_improvement
 
-    def policy_improvement(self):
-        for state in self.states:
-            value = self.value_estimation(state)
 
-    def act(self):
-        actions = self.env.get_actions()
+    def fit(self) -> None:
+        max_iter = 10_000
+        for it in range(max_iter):
+            for idx in itertools.count():
+                if self.estimate_states(self.eps) <= 1e-6:
+                    # print(f"steps to estimate state values: {idx}")
+                    break
+            if not self.improve_policy():
+                # print(f"policy was not improved")
+                if self.eps < 1e-9:
+                    break
+                if self.eps < 1e-6:
+                    self.eps = 0.
+                self.eps /= 10
+
+    def act(self, bank: int) -> int:
+        return self.greedy_actions[bank]
